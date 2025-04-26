504bf588d4f1c293036370a6c83486c8f7a02c22
gitpro-gif <vedantgithub2@gmail.com>
2025-04-26 17:40:11 +0530

    Whole backend

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+const TodoModel = require('./Models/Todo')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get('/get', (req, res) => {
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.post('/add', (req, res) => {
+    const task = req.body.task;
+    TodoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+});
+
+app.put('/update/:id', (req, res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndUpdate({_id: id}, {done: true})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndDelete({_id : id})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on PORT`);
+})
